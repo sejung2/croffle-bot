084a137334db03ed7c7ea4e94428f4801cb877c0
sejung2 <seingdev@gmail.com>
2026-01-30 01:16:23 +0900

    feat: play-command

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,19 +1,54 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Command } from '@sapphire/framework';
+import { Args, Command } from '@sapphire/framework';
+import { useMainPlayer } from 'discord-player';
 import type { Message } from 'discord.js';
 
-// Not Implemented Yet
 @ApplyOptions<Command.Options>({
 	description: 'Plays music in your current voice channel',
-	aliases: ['p'] // Adding an alias commands for the play command
+	aliases: ['p', 'play'] // Adding an alias commands for the play command
 })
 export class UserCommand extends Command {
-	public override async messageRun(message: Message) {
+	public override async messageRun(message: Message, args: Args) {
+		const { member, guild } = message;
+
+		if (!guild) {
+			return message.reply('This command can only be used in a server.');
+		}
+
+		const voiceChannel = member?.voice.channel;
+		if (!voiceChannel) {
+			return message.reply('You need to be in a voice channel to use this command.');
+		}
+
 		if (!message.channel.isSendable()) {
 			return;
 		}
 
-		// need to implement music play logic here
-		return message.channel.send('Music Play Command Executed!');
+		const query = await args.rest('string').catch(() => null);
+
+		if (!query) {
+			return message.reply('You need to input URL to play music.\nex: `!play <YouTube URL>`');
+		}
+
+		const player = useMainPlayer();
+
+		try {
+			const feedbackMessage = await message.channel.send(`Searching for \`${query}\`...`);
+
+			const { track, queue } = await player.play(voiceChannel, query, {
+				nodeOptions: {
+					metadata: message
+				}
+			});
+
+			if (queue.isPlaying() && queue.tracks.size > 0) {
+				return feedbackMessage.edit(`\`${track.title}\` has been added to the queue! (position: ${queue.tracks.size})`);
+			} else {
+				return feedbackMessage.edit(`\`${track.title}\` is now playing!`);
+			}
+		} catch (error) {
+			console.error('Error playing music:', error);
+			return message.channel.send(`Error occurred while trying to play music: ${error}`);
+		}
 	}
 }
